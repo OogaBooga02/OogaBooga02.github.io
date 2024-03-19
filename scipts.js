bbee1c010c7596948e7f14ba97bc2a0ce77de0b1
OogaBooga02 <83483469+OogaBooga02@users.noreply.github.com>
2024-03-18 19:31:27 -0500

    Add files via upload

--- scipts.js
+++ scipts.js
@@ -1,1 +1,16 @@
+$(document).ready(function() {
+    $('.mobile-button a').click(function(e) {
+        $(this).parent().parent().toggleClass('open');
+        $(this).html($(this).html() == 'Close Menu' ? 'Menu' : 'Close Menu');
+        e.preventDefault();
+    });
 
+    function toggleProjectDetails(projectId) {
+        var projectDetails = document.getElementById(projectId).getElementsByClassName("project-details")[0];
+        if (projectDetails.style.display === "none") {
+            projectDetails.style.display = "block";
+        } else {
+            projectDetails.style.display = "none";
+        }
+    }
+});
